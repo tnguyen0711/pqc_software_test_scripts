718f59638a4f23f8d28acb6b1a071e2defafc6a2
tnguyen0711 <taithomasa@gmail.com>
2025-02-26 13:17:59 -0600

    Delete xmss_sign simplified.py

--- xmss_sign simplified.py
+++
@@ -1,9 +0,0 @@
-def xmss_sign(self, m, sk_seed, idx):
-    """ Simplified XMSS signature generation. """
-    auth = b''
-    for j in range(self.hp):
-        k = (idx >> j) ^ 1
-        auth += self.xmss_node(sk_seed, k, j)
-    sig = self.wots_sign(m, sk_seed)
-    sig_xmss = sig + auth
-    return sig_xmss
